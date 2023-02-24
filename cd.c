eb92ab8beed34494542bd3ec34f6143287f2930a
Bart254 <omondibartlett3@gmail.com>
2023-02-24 23:49:24 +0300

    update cd

--- cd.c
+++ cd.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * cd - changes to a new directory
@@ -14,9 +15,16 @@ void cd(char **args, int status __attribute__((unused)))
 
 	cwd = getcwd(buffer, size);
 	if (args[1] == NULL)
+	{
 		new_d = getenv("HOME");
+		if (new_d == NULL)
+			return;
+	}
 	else if (strcmp(args[1], "-") == 0)
+	{
 		new_d = getenv("OLDPWD");
+		printf("%s\n", new_d);
+	}
 	else
 		new_d = args[1];
 	chdir(new_d);
